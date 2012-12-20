4fc787a433e56176978ec50625e1fc3c573d7b21
Ralf Wehner <Ralf.Fred@googlemail.com>
2012-12-20 22:41:52 +0100

    fix junit test caused by undefined/ different locales

--- modules/mm-conductor/src/test/java/de/wehner/mediamagpie/common/util/TimeUtilTest.java
+++ modules/mm-conductor/src/test/java/de/wehner/mediamagpie/common/util/TimeUtilTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import org.junit.Test;
 
@@ -15,19 +16,18 @@ public class TimeUtilTest {
 
     @Test
     public void testBuildFileNameWithTimeStamp() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.setTimeInMillis(0);
-        calendar.set(2010, 4, 20);
+        Calendar calendar = new GregorianCalendar(Locale.US);
+        calendar.set(2010, 4, 20, 8, 5, 17);
         Date date = calendar.getTime();
-        assertEquals(new File("a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("a.txt"), date, null));
-        assertEquals(new File("12100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("12.txt"), date, null));
-        assertEquals(new File("/a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("/a.txt"), date, null));
-        assertEquals(new File("./a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("./a.txt"), date, null));
-        assertEquals(new File(".a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File(".a.txt"), date, null));
-        assertEquals(new File("path/a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("path/a.txt"), date, null));
-        assertEquals(new File("/path/a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("/path/a.txt"), date, null));
+        assertEquals(new File("a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("a.txt"), date, null));
+        assertEquals(new File("12100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("12.txt"), date, null));
+        assertEquals(new File("/a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("/a.txt"), date, null));
+        assertEquals(new File("./a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("./a.txt"), date, null));
+        assertEquals(new File(".a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File(".a.txt"), date, null));
+        assertEquals(new File("path/a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("path/a.txt"), date, null));
+        assertEquals(new File("/path/a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("/path/a.txt"), date, null));
         if (File.pathSeparator.equals("\\")) {
-            assertEquals(new File("d:\\path\\a100520_010000.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("d:\\path\\a.txt"), date, null));
+            assertEquals(new File("d:\\path\\a100520_080517.txt"), TimeUtil.buildFileNameWithTimeStamp(new File("d:\\path\\a.txt"), date, null));
         }
     }
 }
