092a83adb90ed6f9d5815cf6eb85f91aae951417
Ralf Wehner <epost@ralfwehner.org>
2014-10-10 08:15:07 +0200

    fixed ruby script

--- puppet/src/puppet/modules/ec2/lib/facter/ec2_public_ipv4.rb
+++ puppet/src/puppet/modules/ec2/lib/facter/ec2_public_ipv4.rb
@@ -1,9 +1,10 @@
 # ec2_public_ipv4.rb
 Facter.add("ec2_public_ipv4") do
     setcode do
-        public_ip = Facter::Util::Resolution.exec("curl -s http://169.254.169.254/latest/meta-data/public-ipv4 --max-time 1")
+        public_ip = Facter::Util::Resolution.exec("curl -s http://169.254.169.254/latest/meta-data/public-ipv4 --max-time 1") 
         if public_ip.to_s == ''
             public_ip = "unknown"
         end
+        public_ip
     end
 end
